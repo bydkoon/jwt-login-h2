8fcfe6acac008f06394535b97596c9a2ad23ce98
k <k@MacBook-Pro-2.local>
2022-07-07 23:29:32 +0900

    #1 h2 connection and console

---
+++ api/src/test/java/com/es/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
