5e951af44a9c729d6c0e9be3db3931851367f5d9
dohoon kim <pansew@gmail.com>
2022-07-08 20:51:44 +0900

    #2 swagger , h2 data, schema 추가 / properties -> yml

---
+++ api/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS USER;
+CREATE TABLE USER
+(
+    userId  VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    `name`   VARCHAR(255),
+    regNo    VARCHAR(255),
+    PRIMARY KEY (userId)
+);
