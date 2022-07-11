5f6206cbec60a1b1874aa219a9a047b75ec12f75
dohoon kim <pansew@gmail.com>
2022-07-11 17:38:25 +0900

    #3 base resources 수정

--- api/src/main/resources/schema.sql
+++ api/src/main/resources/schema.sql
@@ -2,10 +2,11 @@ DROP TABLE IF EXISTS USER;
 
 CREATE TABLE USER
 (
-    id BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'ID1',
+    id bigint NOT NULL auto_increment,
     userId  VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     `name`   VARCHAR(255),
-    regNo    VARCHAR(255) NOT NULL
+    regNo    VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
+
 );
