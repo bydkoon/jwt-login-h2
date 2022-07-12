71f648448606f8ba31c8140e8839392ff6c9369c
dohoon kim <pansew@gmail.com>
2022-07-12 22:47:11 +0900

    security 수정

--- api/src/main/java/com/es/api/config/security/UserPrincipal.java
+++ api/src/main/java/com/es/api/config/security/UserPrincipal.java
@@ -1,50 +1,72 @@
 package com.es.api.config.security;
 
-import lombok.Getter;
+import com.es.api.dto.UserDTO;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.SpringSecurityCoreVersion;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
-//@Getter
-//public class UserPrincipal implements UserDetails {
-//
-//    private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
-//    private String id;	// DB에서 PK 값
-//    private String name;		// 로그인용 ID 값
-//    private String password;	// 비밀번호
-//    private String regNo;	//주민번호
-//    private Collection<GrantedAuthority> authorities;	//권한 목록
-//
-////    public UserPrincipal(User user) {
-////        this.user = user;
-////    }
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return authorities;
-//    }
-//    @Override
-//    public String getUsername() {
-//        return id;
-//    }
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//
-//    }
-//}
+public class UserPrincipal implements UserDetails {
+    private final UserDTO userDTO;
+
+    public UserPrincipal(UserDTO userDTO){
+        this.userDTO = userDTO;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        // Extract list of permissions (name)
+        this.userDTO.getPermissionList().forEach(p -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority(p);
+            authorities.add(authority);
+        });
+
+        // Extract list of roles (ROLE_name)
+        this.userDTO.getRoleList().forEach(r -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + r);
+            authorities.add(authority);
+        });
+
+        return authorities;
+    }
 
 
+    public long getID() {
+        return this.userDTO.getId();
+    }
+    @Override
+    public String getPassword() {
+        return this.userDTO.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userDTO.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+}
